fc39ec1e49e01b55ae947863ead13bbab90963e4
mehdi89 <mehedi.hasan@oisbd.net>
2015-02-01 20:24:01 +0600

    kendo report controller setup

--- static/appScript/app.js
+++ static/appScript/app.js
@@ -24,6 +24,7 @@ angular.module('project', ['ui.bootstrap', 'ngGrid', 'jQuery-ui']).
                     when('/transaction', {templateUrl: BASE_URL + 'transaction_ctrl'}).
                     when('/Users', {templateUrl: BASE_URL + 'Users_ctrl'}).
                     when('/std_report', {templateUrl: BASE_URL + 'std_report_ctrl'}).
+                    when('/std_report_kendo', {templateUrl: BASE_URL + 'std_report_kendo_ctrl'}).
                     when('/std_report/single/:page', {
                         templateUrl: function ($routeParams) {
                             return BASE_URL + 'std_report_ctrl/single/' + $routeParams.page + '';
